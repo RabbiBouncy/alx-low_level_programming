7223ea7299f7d724ada51b71829b633ffbf5f714
RabbiBouncy <rabbia334@gmail.com>
2023-07-27 11:17:34 -0700

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,9 +3,10 @@
  * reverse_array - reverse array of integers
  * @a: array
  * @n: number of elements of array
+ *
  * Return: void
  */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
 	int i;
 	int t;
